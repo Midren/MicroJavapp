07ef6d0c8e82f4d136e95035ca0e126e6186a27b
Hermann Yavorskyi <yavorskyi@ucu.edu.ua>
2020-02-09 05:02:40 +0200

    Fixed Truffleboundary in read node

--- code/org.truffle.cs.mj/src/org/truffle/cs/mj/nodes/MJReadNode.java
+++ code/org.truffle.cs.mj/src/org/truffle/cs/mj/nodes/MJReadNode.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.truffle.cs.mj.parser.identifiertable.types.TypeDescriptor;
 
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
@@ -18,17 +19,20 @@ public class MJReadNode extends MJStatementNode {
         this.frameSlot = frameSlot;
     }
 
-    @Override
-    public Object execute(VirtualFrame frame) {
-        char c;
+    @TruffleBoundary
+    private static char scanChar() {
         try {
-            c = (char) System.in.read();
+            return (char) System.in.read();
         } catch (IOException e) {
             CompilerDirectives.transferToInterpreterAndInvalidate();
             e.printStackTrace();
-            return null;
+            throw new Error("Error while reading from keyboard");
         }
-        MJVariableNodeFactory.MJWriteLocalVariableNodeGen.create(MJConstantNodeFactory.CharNodeGen.create(c), frameSlot, typeDescriptor).execute(frame);
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        MJVariableNodeFactory.MJWriteLocalVariableNodeGen.create(MJConstantNodeFactory.CharNodeGen.create(scanChar()), frameSlot, typeDescriptor).execute(frame);
         return null;
     }
 
